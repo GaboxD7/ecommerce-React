3f9fdfaeb01633c5bf8d97f57e75e84b370c6e39
Gabriel <alej.gabriel.14@gmail.com>
2022-06-06 12:29:42 -0400

    CONTADOR CON BOTON-WITH-ADD

--- src/App.js
+++ src/App.js
@@ -8,11 +8,18 @@ import Navbar from './Components/Navbar.jsx';
 
 
 function App() {
+
+    const onAdd = (count) => {
+      alert(`Agregaste ${count} productos `)
+    }
   return (
     <div className="App">
       <header className="App-header">
       <Navbar></Navbar>
-      <ItemCount value={1}></ItemCount>
+      <ItemCount 
+      value={1}
+      max={10}
+      onAdd={onAdd}></ItemCount>
       <ItemListContainer/>
               </header>
     </div>
--- src/Components/ItemCount.jsx
+++ src/Components/ItemCount.jsx
@@ -1,36 +1,46 @@
 import React from 'react';
 import { useState } from 'react';
 import './ItemCount.css';
-const ItemCount = ({value}) => {
+const ItemCount = ({value, max, onAdd}) => {
     console.log(value)
 
 const [count, setCount] = useState(value);
 
 const restar = () => { 
 
-
-    if ( count > 0) {
-        setCount(count - 1);
-    }
+count > value ?  setCount(count - 1) : alert('No puedes quitar mas productos')
+  
+       
+  
 }
 const sumar = () => {
-    setCount(count + 1);
-}
 
+count < max ? setCount(count + 1) :  alert('No puedes add mas producto');
+
+}
+const reset = () => {
+    setCount(value);
+}
   return (
       <>
           <div className='contenedor-count'>
               <div className='contenedor-btn-count'>
-                  <button onClick={restar}>-</button> 
+                  <button 
+                  onClick={restar}>-</button> 
                   <h2>{count}</h2>
-                  <button onClick={sumar}>+</button>
-                  
+                  <button 
+                  onClick={sumar}>+</button>
               </div>
-              <button className='btn-agregar'>AGREGAR AL CARRITO</button>
+                  <button 
+                     className='btn-agregar'
+                     onClick={() => {onAdd(count); reset() }  }
+         
+                     >AGREGAR AL CARRITO
+                </button>
           </div>
       </>
 
   )
 }
 
-export default ItemCount
+export default ItemCount;
