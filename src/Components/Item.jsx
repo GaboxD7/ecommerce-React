6cde43c1d80f9cfdd19020f599e4bfe8ab9ac95f
Gabriel <alej.gabriel.14@gmail.com>
2022-06-17 00:07:11 -0400

    Primera-Entrega-and-correction-Detalle-de-Producto

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -7,7 +7,7 @@ import './Item.css'
 const Item = ({bike}) => {
 
 
-  const {name, image, brand, model, price, id } = bike
+  const {name, image, brand, model, price, id, category } = bike
 
   return (
     <div className='col-12 col-sm-6 col-md-4 col-lg-3'>
@@ -15,8 +15,9 @@ const Item = ({bike}) => {
         <img src={image} alt="Avatar"  />
           <div className="container-contenido">
             <h6><b>{name}</b></h6>
-            <p>Precio: {` $ ${price} `} </p>
-            <p>{brand} {model}</p>
+            <h5>Precio: {` $ ${price} `} </h5>
+            <p className='p-model'>{brand} {model}</p>
+            <p className='p-categoria'>CATEGORIA: {category}</p>
           </div>
             <Link to={`/item/${id}`} className="btn-ver text-center">VER DETALLES</Link>
       </div>
