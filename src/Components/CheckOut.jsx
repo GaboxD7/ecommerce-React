5d0802ba4bd5386ac3e64791f43f96c0d66d9cd8
Gabriel <alej.gabriel.14@gmail.com>
2022-07-16 02:03:27 -0400

    Formulario-ready-html-funcionalidad

--- src/Components/CheckOut.jsx
+++ src/Components/CheckOut.jsx
@@ -1,45 +1,133 @@
 import { addDoc, collection, getFirestore } from 'firebase/firestore';
-import React from 'react'
 import { useContext } from 'react';
 import { useState } from 'react'
 import { CartContext } from './context/CartContext';
+import './CheckOut.css' 
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const CheckOut = () => {
-  const [ name, setName ] = useState('');
-  const [ email, setEmail ] = useState('');
-  const [ cel, setCel ] = useState('');
+  const [formSend, setFormSend ] = useState(false);
+
   const {cart, getItemPrice} = useContext(CartContext)
   const total = getItemPrice()
   const db = getFirestore();
   const orderCollection = collection(db, 'orders');
   let fechaActual = new Date ()
 
-  const handleClick = () => {
+  // const handleClick = () => {
 
-    const order = {
-      buyer: { name, email, cel },
-      items: cart,
-      fechaActual,
-      total
-    }
+  //   const order = {
+  //     buyer: { name, email, cel },
+  //     items: cart,
+  //     fechaActual,
+  //     total
+  //   }
   
-    addDoc(orderCollection, order).then(({id}) => {
-        console.log(id)
-    })
-  }
+  //   addDoc(orderCollection, order).then(({id}) => {
+  //       console.log(id)
+  //   })
+  // }
   return (
     <>
-    <section className='d-flex justify-content-center align-items-center vh-100  '>
-      {/* <form action=""> */}
-      <label htmlFor="">Nombre</label>
-      <input type="text" placeholder='Nombre'  onChange={(e) => setName(e.target.value)} />
-      <label htmlFor="">Email</label>
-      <input type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
-      <label htmlFor="">Celular</label>
-      <input type="cel" placeholder='Celular' onChange={(e) => setCel(e.target.value)}  />
-      <button onClick={handleClick}>comprar</button>
-      {/* </form> */}
-    </section>
+      <section className='container-form'>
+        <Formik
+          initialValues={{
+            name:'',
+            cel:'',
+            email:''
+            
+          }}
+          validate= {(valores) => {
+            let errores = {};
+            // Validacion nombre
+            if(!valores.name) {
+                errores.name = 'Porfavor ingresa un nombre'
+            } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.name) ) {
+               errores.name = 'El nombre solop puede tener letras y espacios'
+            }
+            // validacion cell
+            if (!valores.cel) {
+              errores.cel = 'Porfavor ingrese un numero'
+            } else if (!/^[+]*[(]?[0-9]{1,4}[)]?[0-9-\s.]+$/.test(valores.cel)) {
+              errores.cel = 'Ingrese un numero valido'
+            }
+            // validacion correo
+            if(!valores.email) {
+              errores.email = 'Por favor ingresa un email'
+            }else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.email))
+             errores.email = 'El email solo debe contener, letras, numeros, puntos, guiones y guion bajo'
+            
+            return errores;
+          }}
+          onSubmit={(valores, {resetForm}) => {
+            // Base de datos
+            console.log(valores)
+                const order = {
+                buyer: valores,
+                items: cart,
+                fechaActual,
+                total
+              }
+  
+              addDoc(orderCollection, order).then(({id}) => {
+                  console.log(id);
+                  alert("su pedido es "+ id)
+              })
+              
+            resetForm()
+            setFormSend(true)
+            setTimeout(()=> setFormSend(false), 3000 )
+          }}
+        >
+              {({errors}) => (
+                    <Form >
+        
+                        <div>
+                            <label htmlFor='nombre'>Nombre</label>
+                            <Field 
+                            type="text" 
+                            id='nombre' 
+                            name='name' 
+                            placeholder='Escribe tu nombre' 
+            
+                            />
+                             <ErrorMessage name='name' component={() => (
+                             <div className='error'>{errors.name}</div> )} />
+         
+                        </div>
+                        <div>
+                            <label htmlFor='cel'>Celular</label>
+                            <Field 
+                            type="tel" 
+                            id='cel' 
+                            name='cel' 
+                            placeholder='Escribe tu celular' 
+             
+                            />
+                            
+                            <ErrorMessage name='cel' component={() => (
+                              <div className='error'>{errors.cel}</div> )} />
+                        </div>
+                        <div>
+                            <label htmlFor='nombre'>Email</label>
+                            <Field 
+                            type="email" 
+                            id='correo' 
+                            name='email' 
+                            placeholder='Escribe tu correo' 
+  
+
+                            />
+                             <ErrorMessage name='email' component={() => (
+                             <div className='error'>{errors.name}</div> )} />
+                        </div>
+                      <button type='submit' >Enviar</button>
+                      {console.log(formSend)}
+                       {formSend && <p className='exito'>¡Formulario enviado con exito!</p>}
+                  </Form>
+              )}
+        </Formik>
+      </section>
 
     </>
   )
